786b63199c594b8e2a4e5a65b88da35687739cdb
Kyle Ranous <kranous05@gmail.com>
2022-08-22 23:17:04 -0600

    Started Refactor of converting MockClient from a dictionary based class to an in memory sqlite DB

--- mock_client.py
+++ mock_client.py
@@ -1,161 +1,72 @@
 """
-MockClient class used to spoof Docker Python SDK client classes for unit testing.
+mock_client is a temporary db to mock the Docker API in unit tests.
 """
-import json
 
-from docker.errors import APIError
+import sqlite3
+import json
 
 
 
 class MockClient:
-    """
-    MockClient is a class for spoofing the Docker Python SDK for unit testing.
-    """
+
     def __init__(self):
-        self.nodes = MockClient.TestNodes()
+        self._con = sqlite3.connect(':memory:')
+        self._cur = self._con.cursor()
+
+    def build_from_file(self, filename):
+        """
+        Reads in a JSON file of a standard format and populates the database for testing
+        """
+        # Read in the JSON file
+        f = open(filename)
+        table_dict = json.load(f)
+
+        if "swarms" in table_dict:
+            # Build Swarm Table if Needed
+            self._cur.execute('''CREATE TABLE swarms (
+                              id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                              swarm_id TEXT NOT NULL, 
+                              attrs TEXT)''')
+            
+            # Populate Swarm Table from Entries
+            for swarm in table_dict['swarms']:
+                self._cur.execute('''INSERT INTO SWARMS(swarm_id, attrs) VALUES (?,?)''', (swarm['ID'],json.dumps(swarm)))
+
+            self._con.commit() 
+
         
+        # Build Node Table if Needed
+        if "nodes" in table_dict:
+            # Build the Node Table if Needed
+            self._cur.execute('''CREATE TABLE nodes (
+                              id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+                              node_id TEXT, 
+                              node_name TEXT, 
+                              attrs TEXT, 
+                              state TEXT,
+                              swarm_id  INTEGER,
+                              FOREIGN KEY (id)
+                              REFERENCES swarms (id))''')
 
-    def load_from_file(self, file_name):
-        """
-            Creates a MockClient object with Nodes and a swarm from a JSON File
-        """
-        f = open(file_name)
-        client_dict = json.load(f)
-        if client_dict and len(self.nodes.node_list) == 0:
-            # Create Nodes
-            for node in client_dict.get('nodes'):
-                self.nodes.add_node(node)
-            
-            for swarm in client_dict.get('swarms'):
-                self.swarm = self.Swarm(swarm)
-            
-            return True
-        
-        return False
-    class TestNodes:
-        """
-        
-        """
-        node_list = []
+            for node in table_dict['nodes']:
+                # Validate attrs dict
 
-        def add_node(self, node_dict):
-            """
-            """
-            self.node_list.append(MockClient.Node(node_dict))
+                # Get swarm_id
+                swarm_id = self._cur.execute('''SELECT id from swarms WHERE swarm_id = (?)''', (node['swarm_id'],)).fetchone()
+                print(node['swarm_id'])
+                print(swarm_id)
+                # Build Node Tupple
+                node_tup = (node['attrs']['ID'], node['attrs']['Spec']['Name'], json.dumps(node['attrs']), node['state'], swarm_id[0])
+                # Insert Node into Table
+                self._cur.execute('''INSERT INTO NODES(node_id, node_name, attrs, state, swarm_id) VALUES (?,?,?,?,?)''', node_tup)
 
-        def get(self, node_id):
-            """
-            Returns a Node object from either a node id or name.
-            Raises APIError if no node is found.
-            """
-            for node in self.node_list:
-                if node.id == node_id or node.attrs['Spec']['Name'] == node_id:
-                    return node
-
-            # If no node is found with matching id raise docker.errors.APIError
-            raise APIError(f"No node found with id or name {node_id}")
-
-        def list(self, filters=None):
-            """
-            Returns a list of nodes 
-            """
-            nodes = []
-            if filters:
-                if 'role' in filters.keys():
-                    for node in self.node_list:
-                        if node.attrs['Spec']['Role'] == filters['role']:
-                            nodes.append(node)
-
-                if 'name' in filters.keys():
-                    filtered_nodes = nodes if len(nodes) > 0 else self.node_list
-                    for node in filtered_nodes:
-                        if filters['name'].lower() in node.attrs['Spec']['Name'].lower():
-                            nodes.append(node)
-
-                if 'membership' in filters.keys():
-                    filtered_nodes = nodes if len(nodes) > 0 else self.node_list
-                    for node in filtered_nodes:
-                        # Need to figure out the membership filter
-                        pass
-                    
-                if 'id' in filters.keys():
-                    filtered_nodes = nodes if len(nodes) > 0 else self.node_list
-                    for node in filtered_nodes:
-                        if filters['id'].lower() in node.id.lower():
-                            nodes.append(node)
-
-                return nodes
-            
-            return self.node_list
-
-    class Node:
-
-        def __init__(self, node_dict):
-
-            self.attrs = node_dict
-            self.id = node_dict['ID']
-            self.short_id = self.id[0:10]
-            self.version = self.attrs["Version"]["Index"]
-            
-        def update(self, node_spec):
-            
-            if "fail" in self.id:
-                #If node is intended to fail, raise docker.errors.APIError
-                raise APIError(f"Failed to update node {self.id}")
-            
-            self.attrs['Spec'] = node_spec
-            if "reload" in self.id:
-                self.reset_id()
+            self._con.commit()
 
             return True
 
-        def reload(self):
-            """
-            simulates docker.Client.Node.reload()
-            """
-            if "noreload" not in self.id:
-                self.update_id(self.id.replace('fail', 'reload'))
+    def close(self):
+        """
+        Simulates closing the connect by destroying the test(temp) database
+        """
+        self._con.close()
 
-        def update_id(self, new_id):
-            self.id = new_id
-            self.short_id = self.id[0:10]
-            self.attrs['ID'] = new_id
-
-        def reset_id(self):
-            if "noreload" not in self.id:
-                new_id = self.id.replace('reload', 'fail')
-                self.id = new_id
-                self.short_id = self.id[0:10]
-                self.attrs['ID'] = new_id
-
-    
-    class Swarm:
-
-        def __init__(self, swarm_dict=None):
-            self.attrs = swarm_dict
-
-        def init(self):
-            pass
-
-        def get_unlock_key(self):
-            pass
-
-        def join(self, remote_addrs, join_token, listen_addr, advertise_addr, data_path_addr):
-            pass
-
-        def leave(self, force=False):
-            pass
-
-        def unlock(self, key):
-            pass
-
-        def update(self):
-            pass
-
-        def reload(self):
-            pass
-            
-    
-
-
-    
